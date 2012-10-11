6fbb53e5a861f5c525bca9b48b707f629771617b
antoine-tran <tranatuan>
2012-10-11 04:08:44 +0200

    Add Sevenly analytics to ShareCounter

--- snapshots/tuan4j/src/social/tuan/social/ShareCounter.java
+++ snapshots/tuan4j/src/social/tuan/social/ShareCounter.java
@@ -54,6 +54,12 @@ public class ShareCounter {
 	public static final String PRIVATE_TWEET_COUNT_API = 
 			"http://urls.api.twitter.com/1/urls/count.json?url=";
 	
+	/** Sevenly API to Facebook Count Statistic Graph */
+	public static final String FACEBOOK_SEVENLY_COUNT = 
+			"https://graph.facebook.com/fql?q=SELECT%%20like_count,%%20total_count," +
+			"%%20share_count,%%20click_count,%%20comment_count%%20FROM%%20link_stat%%20" +
+			"WHERE%%20url%%20=%%20%%22%s%%22";
+	
 	/** Google Plus Share Count API */
 	public static final String GOOGLE_PLUS_CNT_API = "https://clients6.google.com/rpc?key=";
 	
@@ -63,7 +69,7 @@ public class ShareCounter {
 	/** this method returns the number of times a web page ss shared on Facebook 
 	 * @param url the raw HTTP URL address of the web page
 	 * @throws IOException */
-	public static int facebook(String link) throws IOException {
+	public static int facebookShareCnt(String link) throws IOException {
 		
 		// Issue a request to the API
 		String encoded = URLEncoder.encode(link, "ISO-8859-1");
@@ -100,7 +106,7 @@ public class ShareCounter {
 	 * through either a proxy or a direct connection  
 	 * @param url the raw HTTP URL address of the web page
 	 * @throws IOException */
-	public static int facebook(String link, HttpClient client) throws IOException {
+	public static int facebookShareCnt(String link, HttpClient client) throws IOException {
 		
 		// Issue a request to the API
 		String encoded = URLEncoder.encode(link, "ISO-8859-1");
@@ -115,6 +121,85 @@ public class ShareCounter {
 		String countStr = json.substring(shareId + 9, closeBracketId).trim();		
 		try {
 			return Integer.parseInt(countStr); 
+		} catch (NumberFormatException e) {
+			throw new IOException("Mal-formed JSON response: ", e);
+		}		
+	}
+	
+	/** This method returns a number of count statistics on Facebook regarding to a link
+	 * @throws IOException */
+	public static FacebookCount facebookCnt(String link) throws IOException {
+		URL url = new URL(String.format(FACEBOOK_SEVENLY_COUNT, link));
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		InputStream response = conn.getInputStream();
+		InputStreamReader stream = new InputStreamReader(response, "UTF-8");
+		BufferedReader reader = new BufferedReader(stream);
+		String line;
+		StringBuilder output = new StringBuilder();
+		while ((line = reader.readLine()) != null) {
+			output.append(line);
+		}
+		
+		// Release resource
+		reader.close();
+		stream.close();
+		response.close();
+		conn.disconnect();
+		
+		// now extract the count
+		String json = output.toString();
+		try {
+			int offset = json.indexOf("\"like_count\":");
+			int end = json.indexOf(',', offset);
+			String likeStr = json.substring(offset + 13, end).trim();
+			int like = Integer.parseInt(likeStr);
+			
+			offset = json.indexOf("\"share_count\":", end);
+			end = json.indexOf(',', offset);
+			String shareStr = json.substring(offset + 14, end).trim();
+			int share = Integer.parseInt(shareStr);
+			
+			offset = json.indexOf("\"comment_count\":", end);
+			end = json.indexOf('}', offset);
+			String commentStr = json.substring(offset + 16, end).trim();
+			int comment = Integer.parseInt(commentStr);
+			
+			return new FacebookCount(share, like, comment);
+
+		} catch (NumberFormatException e) {
+			throw new IOException("Mal-formed JSON response: ", e);
+		}		
+	}
+	
+	/** This method returns a number of count statistics on Facebook regarding to a link. It can work
+	 * through a proxy
+	 * @throws IOException */
+	public static FacebookCount facebookCnt(String link, HttpClient client) throws IOException {
+		HttpGet req = new HttpGet(String.format(FACEBOOK_SEVENLY_COUNT, link));
+		HttpResponse response = client.execute(req);
+		HttpEntity entity = response.getEntity();	
+			
+		// now extract the count
+		String json = EntityUtils.toString(entity);
+		try {
+			int offset = json.indexOf("\"like_count\":");
+			int end = json.indexOf(',', offset);
+			String likeStr = json.substring(offset + 13, end).trim();
+			int like = Integer.parseInt(likeStr);
+			
+			offset = json.indexOf("\"share_count\":", end);
+			end = json.indexOf(',', offset);
+			String shareStr = json.substring(offset + 14, end).trim();
+			int share = Integer.parseInt(shareStr);
+			
+			offset = json.indexOf("\"comment_count\":", end);
+			end = json.indexOf('}', offset);
+			String commentStr = json.substring(offset + 16, end).trim();
+			int comment = Integer.parseInt(commentStr);
+			
+			return new FacebookCount(share, like, comment);
+
 		} catch (NumberFormatException e) {
 			throw new IOException("Mal-formed JSON response: ", e);
 		}		
@@ -248,6 +333,7 @@ public class ShareCounter {
 		}
 	}
 	
+	/** This method returns the number of times a link is shared on Google Plus */
 	public static int googlePlus(String link, String keyPath, HttpClient client) throws IOException {
 		
 		// Get the private API key
@@ -311,6 +397,7 @@ public class ShareCounter {
 		}
 	}
 	
+	/** This method returns the number of times a link is shared on LinkedIn */
 	public static int linkedIn(String link) throws IOException {
 		
 		// Issue a request to the API
@@ -344,7 +431,7 @@ public class ShareCounter {
 		}		
 	}
 	
-	/** this method returns the number of times a web page ss shared on Facebook. It works
+	/** this method returns the number of times a web page ss shared on LinkedIn. It works
 	 * through either a proxy or a direct connection  
 	 * @param url the raw HTTP URL address of the web page
 	 * @throws IOException */
@@ -359,8 +446,8 @@ public class ShareCounter {
 		// now extract the count
 		String json = EntityUtils.toString(entity);
 		int shareId =  json.indexOf("\"count\":");
-		int closeBracketId = json.indexOf(",", shareId);
-		String countStr = json.substring(shareId + 9, closeBracketId).trim();		
+		int end = json.indexOf(",", shareId);
+		String countStr = json.substring(shareId + 8, end).trim();	
 		try {
 			return Integer.parseInt(countStr); 
 		} catch (NumberFormatException e) {
@@ -368,16 +455,32 @@ public class ShareCounter {
 		}		
 	}
 
+	public static class FacebookCount {
+		public int shareCnt;
+		public int likeCnt;
+		public int commentCnt;
+
+		public FacebookCount(int shareCnt, int likeCnt, int commentCnt) {
+			this.shareCnt = shareCnt;
+			this.likeCnt = likeCnt;
+			this.commentCnt = commentCnt;
+		}
+		
+		@Override
+		public String toString() {
+			return "[" + shareCnt + ", " + likeCnt + ", " + commentCnt + "]";
+		}
+	}
 	
 	// Test routine
-	/* public static void main(String[] args) {	
+	 public static void main(String[] args) {	
 	
 		try {
-			System.out.println(linkedIn(
+			System.out.println(facebookCnt(
 			"http://news.yahoo.com/california-gasoline-prices-set-plunge-spike-ends-004934596.html"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	} */
+	} 
 }
