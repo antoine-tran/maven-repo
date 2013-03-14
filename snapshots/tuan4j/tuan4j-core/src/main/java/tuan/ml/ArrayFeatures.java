08893b538763d15d4d83ba632019f37bae8ab8c8
antoine-tran <tranatuan24@gmail.com>
2013-03-14 20:45:36 +0100

    making ArrayFeatures editable

--- snapshots/tuan4j/tuan4j-core/src/main/java/tuan/ml/ArrayFeatures.java
+++ snapshots/tuan4j/tuan4j-core/src/main/java/tuan/ml/ArrayFeatures.java
@@ -242,6 +242,102 @@ public class ArrayFeatures implements RandomAccessFeatures {
 	private void sort() {
 		
 		// check if re-sort is needed (to avoid one unnecessary array copy)
-		// Arrays.
+		// Arrays.		
+		boolean ascending = true;
+		if (dim <= 1) return;
+		for (int i = 1; i < keys.length && ascending; i++) {
+			ascending &= (keys[i] >= keys[i-1]);
+		}
+		if (!ascending) {
+			sort(keys, vals, 0, dim);
+		}
 	}
+	
+	// The following code snippets are copied from Java openJDK 6-b14 source code,
+	// with some adaptations to make it able to sort two arrays in parallel
+	/**
+     * Sorts the specified sub-array of integers into ascending order.
+     */
+    private static void sort(int x[], double[] y, int off, int len) {
+        // Insertion sort on smallest arrays
+        if (len < 7) {
+            for (int i=off; i<len+off; i++)
+                for (int j=i; j>off && x[j-1]>x[j]; j--)
+                    swap(x, y, j, j-1);
+            return;
+        }
+
+        // Choose a partition element, v
+        int m = off + (len >> 1);       // Small arrays, middle element
+        if (len > 7) {
+            int l = off;
+            int n = off + len - 1;
+            if (len > 40) {        // Big arrays, pseudomedian of 9
+                int s = len/8;
+                l = med3(x, l,     l+s, l+2*s);
+                m = med3(x, m-s,   m,   m+s);
+                n = med3(x, n-2*s, n-s, n);
+            }
+            m = med3(x, l, m, n); // Mid-size, med of 3
+        }
+        int v = x[m];
+
+        // Establish Invariant: v* (<v)* (>v)* v*
+        int a = off, b = a, c = off + len - 1, d = c;
+        while(true) {
+            while (b <= c && x[b] <= v) {
+                if (x[b] == v)
+                    swap(x, y, a++, b);
+                b++;
+            }
+            while (c >= b && x[c] >= v) {
+                if (x[c] == v)
+                    swap(x, y, c, d--);
+                c--;
+            }
+            if (b > c)
+                break;
+            swap(x, y, b++, c--);
+        }
+
+        // Swap partition elements back to middle
+        int s, n = off + len;
+        s = Math.min(a-off, b-a  );  vecswap(x, y, off, b-s, s);
+        s = Math.min(d-c,   n-d-1);  vecswap(x, y, b,   n-s, s);
+
+        // Recursively sort non-partition-elements
+        if ((s = b-a) > 1)
+            sort(x, y, off, s);
+        if ((s = d-c) > 1)
+            sort(x, y, n-s, s);
+    }
+
+    /**
+     * Swaps x[a] with x[b].
+     */
+    private static void swap(int x[], double[] y, int a, int b) {
+        int t = x[a];
+        x[a] = x[b];
+        x[b] = t;
+        double s = y[a];
+        y[a] = y[b];
+        y[b] = s;
+    }
+
+    /**
+     * Swaps x[a .. (a+n-1)] with x[b .. (b+n-1)].
+     */
+    private static void vecswap(int x[], double[] y, int a, int b, int n) {
+        for (int i=0; i<n; i++, a++, b++)
+            swap(x, y, a, b);
+    }
+
+    /**
+     * Returns the index of the median of the three indexed integers.
+     */
+    private static int med3(int x[], int a, int b, int c) {
+        return (x[a] < x[b] ?
+                (x[b] < x[c] ? b : x[a] < x[c] ? c : a) :
+                (x[b] > x[c] ? b : x[a] > x[c] ? c : a));
+    }
 }
