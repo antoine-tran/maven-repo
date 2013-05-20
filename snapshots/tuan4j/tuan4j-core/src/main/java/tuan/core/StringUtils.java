04979bbb2ae98a866fc7ba740d4481338d6d0caf
antoine-tran <tranatuan>
2013-05-20 11:41:04 +0200

    add wikiUrlEncode to tuan4j-core

--- snapshots/tuan4j/tuan4j-core/src/main/java/tuan/core/StringUtils.java
+++ snapshots/tuan4j/tuan4j-core/src/main/java/tuan/core/StringUtils.java
@@ -17,8 +17,13 @@
  */
 package tuan.core;
 
+import java.io.CharArrayWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,12 +32,75 @@ import java.util.regex.Pattern;
  * Provide a list of utility methods for strings
  * @author Tuan Tran
  * @author Niket Tandon
+ * @author  Herb Jellinek
  *
  */
 public class StringUtils {
 
 	private static final Pattern NUMERIC_PATTERN = Pattern.compile("-?\\d+(.\\d+)?");
 	private static final Pattern WHITE_SPACE_PATTERN = Pattern.compile("\\s+");
+	private static final int caseDiff = ('a' - 'A');
+    private static BitSet dontNeedEncoding;
+
+	// Source: URLEncode.java, Herb Jellinek
+	static {
+
+        /* The list of characters that are not encoded has been
+         * determined as follows:
+         *
+         * RFC 2396 states:
+         * -----
+         * Data characters that are allowed in a URI but do not have a
+         * reserved purpose are called unreserved.  These include upper
+         * and lower case letters, decimal digits, and a limited set of
+         * punctuation marks and symbols.
+         *
+         * unreserved  = alphanum | mark
+         *
+         * mark        = "-" | "_" | "." | "!" | "~" | "*" | "'" | "(" | ")"
+         *
+         * Unreserved characters can be escaped without changing the
+         * semantics of the URI, but this should not be done unless the
+         * URI is being used in a context that does not allow the
+         * unescaped character to appear.
+         * -----
+         *
+         * It appears that both Netscape and Internet Explorer escape
+         * all special characters from this list with the exception
+         * of "-", "_", ".", "*". While it is not clear why they are
+         * escaping the other characters, perhaps it is safest to
+         * assume that there might be contexts in which the others
+         * are unsafe if not escaped. Therefore, we will use the same
+         * list. It is also noteworthy that this is consistent with
+         * O'Reilly's "HTML: The Definitive Guide" (page 164).
+         *
+         * As a last note, Intenet Explorer does not encode the "@"
+         * character which is clearly not unreserved according to the
+         * RFC. We are being consistent with the RFC in this matter,
+         * as is Netscape.
+         *
+         */
+
+        dontNeedEncoding = new BitSet(256);
+        int i;
+        for (i = 'a'; i <= 'z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = 'A'; i <= 'Z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = '0'; i <= '9'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        dontNeedEncoding.set('-');
+        dontNeedEncoding.set('_');
+        dontNeedEncoding.set('(');
+        dontNeedEncoding.set(')');
+        dontNeedEncoding.set(',');
+        dontNeedEncoding.set('.');
+        dontNeedEncoding.set(':');
+        dontNeedEncoding.set('*');
+    }
 	
 	/** An efficient method to check if a string is about a number */
 	public static boolean isNumeric(String string) {
@@ -274,19 +342,102 @@ public class StringUtils {
 	
 	/** Wikipedia uses a different URL percent-encoding scheme. This method
 	 *  accepts an arbitrary string, and convert it into a mediawiki HTTP
-	 *  compatible path fragment
+	 *  compatible path fragment. It is a standard <code>application/x-www-form-urlencoded</code>
+	 *  supplied scheme except that some reserved characters are unchanged. This
+	 *  methods modifies the URLEncoder.encode() method in Java 1.6 Open JDK
+	 *  
+	 *  @author Herb Jellinek
 	 */
-	public static String wikiUrlEncode(String s) {
-		String encoded;
-		try {
-			encoded = URLEncoder.encode(s, "UTF-8");
-			encoded = encoded.replaceAll("%28", "(");
-			encoded = encoded.replaceAll("%29", ")");
-			encoded = encoded.replaceAll("%2C", ",");
-			encoded = encoded.replaceAll("%3A", ":");
-			return encoded;
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
+	public static String wikiUrlEncode(String s, String enc) 
+			throws UnsupportedEncodingException {
+		
+		boolean needToChange = false;
+        StringBuffer out = new StringBuffer(s.length());
+        Charset charset;
+        CharArrayWriter charArrayWriter = new CharArrayWriter();
+        
+        if (enc == null)
+            throw new NullPointerException("charsetName");
+
+        try {
+            charset = Charset.forName(enc);
+        } catch (IllegalCharsetNameException e) {
+            throw new UnsupportedEncodingException(enc);
+        } catch (UnsupportedCharsetException e) {
+            throw new UnsupportedEncodingException(enc);
+        }
+		
+        
+        for (int i = 0; i < s.length();) {
+            int c = (int) s.charAt(i);
+            //System.out.println("Examining character: " + c);
+            if (dontNeedEncoding.get(c)) {
+                if (c == ' ') {
+                    c = '+';
+                    needToChange = true;
+                }
+                out.append((char)c);
+                i++;
+            } else {
+                // convert to external encoding before hex conversion
+                do {
+                    charArrayWriter.write(c);
+                    /*
+                     * If this character represents the start of a Unicode
+                     * surrogate pair, then pass in two characters. It's not
+                     * clear what should be done if a bytes reserved in the
+                     * surrogate pairs range occurs outside of a legal
+                     * surrogate pair. For now, just treat it as if it were
+                     * any other character.
+                     */
+                    if (c >= 0xD800 && c <= 0xDBFF) {
+                        /*
+                          System.out.println(Integer.toHexString(c)
+                          + " is high surrogate");
+                        */
+                        if ( (i+1) < s.length()) {
+                            int d = (int) s.charAt(i+1);
+                            /*
+                              System.out.println("\tExamining "
+                              + Integer.toHexString(d));
+                            */
+                            if (d >= 0xDC00 && d <= 0xDFFF) {
+                                /*
+                                  System.out.println("\t"
+                                  + Integer.toHexString(d)
+                                  + " is low surrogate");
+                                */
+                                charArrayWriter.write(d);
+                                i++;
+                            }
+                        }
+                    }
+                    i++;
+                } while (i < s.length() && !dontNeedEncoding.get((c = (int) s.charAt(i))));
+
+                charArrayWriter.flush();
+                String str = new String(charArrayWriter.toCharArray());
+                byte[] ba = str.getBytes(charset);
+                for (int j = 0; j < ba.length; j++) {
+                    out.append('%');
+                    char ch = Character.forDigit((ba[j] >> 4) & 0xF, 16);
+                    // converting to use uppercase letter as part of
+                    // the hex value if ch is a letter.
+                    if (Character.isLetter(ch)) {
+                        ch -= caseDiff;
+                    }
+                    out.append(ch);
+                    ch = Character.forDigit(ba[j] & 0xF, 16);
+                    if (Character.isLetter(ch)) {
+                        ch -= caseDiff;
+                    }
+                    out.append(ch);
+                }
+                charArrayWriter.reset();
+                needToChange = true;
+            }
+        }
+
+        return (needToChange? out.toString() : s);
 	}
 }
