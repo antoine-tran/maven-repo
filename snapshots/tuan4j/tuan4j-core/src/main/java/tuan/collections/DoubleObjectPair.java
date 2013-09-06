fb05bc4b45008fe57012387bd6c842a4ec042eaf
antoine-tran <tranatuan24@gmail.com>
2013-09-06 17:10:39 +0200

    add DoubleObjectPair

---
+++ snapshots/tuan4j/tuan4j-core/src/main/java/tuan/collections/DoubleObjectPair.java
@@ -0,0 +1,53 @@
+package tuan.collections;
+
+import java.io.Serializable;
+
+/** An entry of key-value pair */
+@SuppressWarnings("serial")
+public class DoubleObjectPair<T extends Serializable> implements Serializable {
+	
+	private int key;
+	private T val;
+	
+	public DoubleObjectPair(int key, T value) {
+		this.key = key;
+		this.val = value;
+	}
+	
+	public int first() {
+		return key;
+	}
+
+	public T second() {
+		return val;
+	}
+	
+	public void setFirst(int key) {
+		this.key = key;
+	}
+	
+	public void setSecond(T val) {
+		this.val = val;
+	}
+		
+	@Override
+	public String toString() {
+		return key + "=" + val;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if (obj == null) return false;
+		if (!(obj instanceof DoubleObjectPair<?>)) return false;
+		DoubleObjectPair<T> iop = (DoubleObjectPair<T>)obj;
+		return ((key == iop.key) && 
+				((val == null && iop.val == null) || val.equals(iop.val)));
+	}
+	
+	@Override
+	public int hashCode() {
+		return (val == null) ? key : key * 31 + val.hashCode();
+	}
+}
