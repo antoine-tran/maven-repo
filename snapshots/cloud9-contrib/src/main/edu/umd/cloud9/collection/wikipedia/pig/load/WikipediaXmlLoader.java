e069b78457470261d8110dfcc82b5e7eed1196d0
antoine-tran <tranatuan24@gmail.com>
2014-03-28 18:19:18 +0100

    inherit schema from pignlproc

--- snapshots/cloud9-contrib/src/main/edu/umd/cloud9/collection/wikipedia/pig/load/WikipediaXmlLoader.java
+++ snapshots/cloud9-contrib/src/main/edu/umd/cloud9/collection/wikipedia/pig/load/WikipediaXmlLoader.java
@@ -14,7 +14,10 @@ import org.apache.pig.LoadMetadata;
 import org.apache.pig.ResourceSchema;
 import org.apache.pig.ResourceStatistics;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
+import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 
 import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
 import edu.umd.cloud9.collection.wikipedia.WikipediaPageInputFormat;
@@ -27,13 +30,13 @@ import edu.umd.cloud9.collection.wikipedia.WikipediaPageInputFormat;
 public class WikipediaXmlLoader extends LoadFunc implements LoadMetadata {
 
 	private RecordReader<LongWritable, WikipediaPage> reader; 
-	
+
 	// do not create too many location paths, since setLocation() will be called by Pig many times
 	private Path path = null;
-	
+
 	// a cached object that defines the output schema of a Wikipedia page
 	private ResourceSchema schema;
-	
+
 	@SuppressWarnings("rawtypes")
 	@Override
 	public InputFormat getInputFormat() throws IOException {
@@ -46,7 +49,7 @@ public class WikipediaXmlLoader extends LoadFunc implements LoadMetadata {
 			throws IOException {
 		this.reader = (RecordReader<LongWritable, WikipediaPage>)reader;		
 	}
-	
+
 	@Override
 	public Tuple getNext() throws IOException {
 		boolean hasNext;
@@ -63,9 +66,9 @@ public class WikipediaXmlLoader extends LoadFunc implements LoadMetadata {
 
 	@Override
 	public void setLocation(String loc, Job job) throws IOException {
-		
+
 		// TODO: Test this and path.getName(), or find documentation specifying which one
-				// should be used here
+		// should be used here
 		if (path == null || !path.toString().equals(loc)) {
 			path = new Path(loc);
 			FileInputFormat.setInputPaths(job, path);
@@ -81,7 +84,7 @@ public class WikipediaXmlLoader extends LoadFunc implements LoadMetadata {
 	@Override
 	public ResourceSchema getSchema(String loc, Job job) throws IOException {
 		if (schema == null) {
-			
+			defineSchema();
 		}
 		return schema;
 	}
@@ -96,6 +99,40 @@ public class WikipediaXmlLoader extends LoadFunc implements LoadMetadata {
 	@Override
 	public void setPartitionFilter(Expression arg0) throws IOException {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	private void defineSchema() {
+		Schema schema = new Schema();
+		schema.add(new FieldSchema("title", DataType.CHARARRAY));
+		schema.add(new FieldSchema("id", DataType.CHARARRAY));
+		schema.add(new FieldSchema("text", DataType.CHARARRAY));
+		schema.add(new FieldSchema("redirect", DataType.CHARARRAY));
+
+		// wrapping each bag in a tuple
+
+		Schema linkInfoSchema = new Schema();
+		linkInfoSchema.add(new FieldSchema("target", DataType.CHARARRAY));
+		linkInfoSchema.add(new FieldSchema("begin", DataType.INTEGER));
+		linkInfoSchema.add(new FieldSchema("end", DataType.INTEGER));
+		Schema linkInfoWrapper = new Schema(new FieldSchema ("t", linkInfoSchema));
+		linkInfoWrapper.setTwoLevelAccessRequired(true);
+		// schema.add(new FieldSchema("links", linkInfoWrapper, DataType.BAG));
+
+		Schema headerInfoSchema = new Schema();
+		headerInfoSchema.add(new FieldSchema("tagname", DataType.CHARARRAY));
+		headerInfoSchema.add(new FieldSchema("begin", DataType.INTEGER));
+		headerInfoSchema.add(new FieldSchema("end", DataType.INTEGER));
+		Schema headerInfoWrapper = new Schema(new FieldSchema("t", headerInfoSchema));
+		headerInfoWrapper.setTwoLevelAccessRequired(true);
+		// schema.add(new FieldSchema("headers", headerInfoWrapper, DataType.BAG));
+
+		Schema paragraphInfoSchema = new Schema();
+		paragraphInfoSchema.add(new FieldSchema("tagname", DataType.CHARARRAY));
+		paragraphInfoSchema.add(new FieldSchema("begin", DataType.INTEGER));
+		paragraphInfoSchema.add(new FieldSchema("end", DataType.INTEGER));
+		Schema paragraphInfoWrapper = new Schema(new FieldSchema("t", paragraphInfoSchema));
+		paragraphInfoWrapper.setTwoLevelAccessRequired(true);
+		// schema.add(new FieldSchema("paragraphs", paragraphInfoWrapper, DataType.BAG));
 	}
 }
