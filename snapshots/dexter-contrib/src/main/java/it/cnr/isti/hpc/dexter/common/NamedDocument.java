2a32f0f598caf8cba64bb786501232bf3eb57540
antoine-tran <tranatuan24@gmail.com>
2015-03-31 18:59:49 +0200

    publish dexter-contrib into the maven-repo

---
+++ snapshots/dexter-contrib/src/main/java/it/cnr/isti/hpc/dexter/common/NamedDocument.java
@@ -0,0 +1,13 @@
+package it.cnr.isti.hpc.dexter.common;
+
+/**
+ * A named document is just a flat document with identifier
+ * @author tuan
+ *
+ */
+public class NamedDocument extends FlatDocument {
+
+	private String docname;
+	
+	public NamedDocument() {}
+}
---
+++ snapshots/dexter-contrib/src/main/java/it/cnr/isti/hpc/dexter/spotter/ManualSpotter.java
@@ -0,0 +1,13 @@
+package it.cnr.isti.hpc.dexter.spotter;
+
+/**
+ * A "manual spotter" that simply reads the ground truth of human annotations to
+ * build the spots. The tagger that uses this spotter will be just a disambiguator
+ * with a "perfect" spotter 
+ * 
+ * @author tuan
+ *
+ */
+public class ManualSpotter {
+
+}
